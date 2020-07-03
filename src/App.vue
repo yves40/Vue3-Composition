6f4744e2c5ed7a8f3a56f993609e5b8038baa333
yves40 <yves.toubhans@free.fr>
2020-07-03 19:12:27 +0200

    Start cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <template>
-  <p>Multiple root components</p>
   <div id="app">
     <h1>Learning Vue3 composition</h1>
-    <h3>Version is {{ Version }}</h3>
-    <p>Capacity: {{ capacity }}</p>
-    <p>Developers are : {{ devnumber }}</p>
-    <ul>
-      <li v-for="(name, index) in developers" :key="index">
-        {{ name }}
-      </li>
-    </ul>
-    <button @click="increaseCapacity">Increase</button>
+    <h3>{{ Version }}</h3>
     <calculatorRef/>
     <calculatorReactive/>
     <calculatorComputed/>
@@ -36,23 +27,14 @@ export default {
   },
 
   setup() {
-      const count = ref(0);
-      const capacity = ref(3);
-      const Version = "App.vue 1.31, Jul 03 2020";
-      const developers = ref(["Yves", "Isabelle", "Barbilec", "Bintoul", "Ratoon", "Tono"]);
-      const devnumber = computed(() => {
-        return developers.value.length;
-      })
-      function increaseCapacity() { 
-        capacity.value++;
-      }
-      watchEffect( () => {
-      });
+      const Version = "App.vue 1.32, Jul 03 2020";
       onMounted( () => { 
         console.log('Mounted') ;
       });      
-      onUpdated( () => { console.log('Updated: ' + capacity.value)});
-      return { capacity, increaseCapacity, Version, developers, devnumber };
+      onUpdated( () => { 
+        console.log('Updated: ')
+      });
+      return { Version };
     }
   };
 
