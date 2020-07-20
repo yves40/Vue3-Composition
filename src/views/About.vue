ecf30922f5c131ef87b18a87cb97f4de664a6f8d
yves40 <yves.toubhans@free.fr>
2020-07-20 11:49:28 +0200

    Update ENV

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>Learn Vue3</h1>
+    <p>
+      Test some new Vue3 API features<br>
+      Ref, Computed, Reactive, Refs, etc.
+    </p>
+  </div>
+</template>
